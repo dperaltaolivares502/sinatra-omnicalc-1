0854f4e993de4a7e8668e40c060ec7ca0986d55d
dperaltaolivares502 <143421071+dperaltaolivares502@users.noreply.github.com>
2024-02-15 15:33:01 +0000

    hhhh

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  "go to /sqaure/new"
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f / 100
+  @number_of_years = params.fetch("number_of_years").to_i
+  @principal = params.fetch("principal").to_f
+
+  @num_periods = @number_of_years*12
+
+  r = @apr/12
+
+  numerator = r * @principal
+  denominator = 1 - (1 + r) ** -@num_periods
+  
+  @payment = numerator/denominator
+
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  erb(:random_results)
 end
